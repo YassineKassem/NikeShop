85f1b6891909edf3111d94cc148ae31a986c74dd
YassineKassem <yassinekassem33@gmail.com>
2024-10-08 11:30:57 +0100

    docker compose

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/produits/';  // FastAPI backend URL
+// const API_URL = 'http://127.0.0.1:8000/produits/';  // FastAPI backend URL
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/produits/';
 
 // Function to fetch products
 export const fetchProducts = async () => {
