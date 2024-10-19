05f07d2856e932ade2ede7b909923b069c2fcc9f
YassineKassem <yassinekassem33@gmail.com>
2024-10-19 02:22:05 +0100

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'http://127.0.0.1:8000/produits/';  // FastAPI backend URL
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'http://127.0.0.1:8000/produits/';  // FastAPI backend URL
+//const API_URL = import.meta.env.VITE_API_URL;
 
 // Function to fetch products
 export const fetchProducts = async () => {
