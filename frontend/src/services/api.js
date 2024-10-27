116dc6ee4796e45f40e9c47ebc88fbd4b146687d
YassineKassem <yassinekassem33@gmail.com>
2024-10-27 14:48:50 +0100

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://backend:8000/produits/';  // FastAPI backend URL
+const API_URL = import.meta.env.VITE_API_URL || 'http://backend:8000/produits';
 //const API_URL = import.meta.env.VITE_API_URL;
 
 // Function to fetch products
