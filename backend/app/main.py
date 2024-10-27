f62206b97769623df6d171c3a01126b83119397a
YassineKassem <yassinekassem33@gmail.com>
2024-10-27 13:30:15 +0100

    update middleware and services

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Allow CORS for the frontend (localhost:5173 in this case)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React frontend URL
+    allow_origins=["*"],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/produits/';  // FastAPI backend URL
+const API_URL = 'http://backend:8000/produits/';  // FastAPI backend URL
 //const API_URL = import.meta.env.VITE_API_URL;
 
 // Function to fetch products
